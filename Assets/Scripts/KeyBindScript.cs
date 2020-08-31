2662c4154856452478d60b3f6c0e3647e13fc63b
Skully <69292508+Skully-13@users.noreply.github.com>
2020-09-01 04:02:19 +1000

    KeyBinds, comments and error solving
    Created a solution to user assigning preassigned keybinds, may incorporate it diffirently in the future. Commented KeyBinds script, null references resolved.

--- Assets/Scripts/KeyBindScript.cs
+++ Assets/Scripts/KeyBindScript.cs
@@ -7,18 +7,23 @@ public class KeyBindScript : MonoBehaviour
 {
     public static Dictionary<string, KeyCode> keys = new Dictionary<string, KeyCode>(); //where a key(string) returns a value(KeyCode)
 
-    public Text up, down, left, right, jump; //creates a bunch of text variables, up is one down is another one etc
+    //we store keycodes as strings in these Text variables, then use these as the text in the buttons, therefore buttons show keycode for action.
+    //can be found on Keybinds menu with referrence variables matching the names below
+    public Text up, down, left, right, jump; //creates a bunch of text variables, up is one, down is another one etc, SAME AS CREATING 5 DIFFIRENT VARIABLES
 
-    private GameObject currentKey;
+    private GameObject currentKey; //Used to store clicked key clickKey
     //values between 0-255
-    public Color32 changedKey = new Color32(39,171,249,255);//This one is blue
-    public Color32 selectedKey = new Color32(239, 116, 36, 255);//this one is orange
+    public Color32 changedKey = new Color32(39,171,249,255);//This one by default is blue
+    public Color32 selectedKey = new Color32(239, 116, 36, 255);//this one by default is orange
+    public Color32 unassignedKey = new Color32(255, 0, 0, 255); //This sis red by default
 
 
     private void Start()
     {
-        //keys.Add("Up", KeyCode.W);
-        keys.Add("Up", (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("Up", "W"))); //creating a keycode, by setting up a keycode value by converting Up value to string, if the value of Up doesn't exist, default to W
+        //Initialises the dictonary Keys by adding the typical elements below
+        //the string/Key returns the KeyCode./Value
+        keys.Add("Up", KeyCode.W);
+        //keys.Add("Up", (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("Up", "W"))); //creating a keycode, by setting up a keycode value by converting Up value to string, if the value of Up doesn't exist, default to W
         keys.Add("Down", KeyCode.S);
         keys.Add("Left", KeyCode.A);
         keys.Add("Right", KeyCode.D);
@@ -27,8 +32,8 @@ public class KeyBindScript : MonoBehaviour
 
         //keys["Up"] = KeyCode.W; // to have a deafault/reset keys option
 
-        //sets this up in the beginning
-        up.text = keys["Up"].ToString();
+        //using the keys.Add("Key", Value/KeyCode);'s above we put the default dictonaries values into the text values of the buttons
+        up.text = keys["Up"].ToString(); //I.E. the up button text now equals the Keys dictonary of Up which is a value of KeyCode.W, which converted to string is simply "W".
         down.text = keys["Down"].ToString();
         left.text = keys["Left"].ToString();
         right.text = keys["Right"].ToString();
@@ -38,6 +43,7 @@ public class KeyBindScript : MonoBehaviour
     private void Update()
     {
         //instead of Input.GetKeyDown(KeyCode.W), we use the dictonary 'keys' with value "Up" to call what KeyCode is required to be pressed for said action
+        //Although the KeyBindScript. part on the line is not required in this example, as we are in that script already, we have coded it this way so if we copy the code to run characterController in the game scene, it will still work.
         if (Input.GetKeyDown(KeyBindScript.keys["Up"]))//(Keycode.W)
         {
             Debug.Log("Character moves forward");
@@ -45,7 +51,6 @@ public class KeyBindScript : MonoBehaviour
         if (Input.GetKeyDown(KeyBindScript.keys["Down"]))//(Keycode.S)
         {
             Debug.Log("Character moves backward");
-
         }
         if (Input.GetKeyDown(KeyBindScript.keys["Left"]))//(Keycode.A)
         {
@@ -64,55 +69,87 @@ public class KeyBindScript : MonoBehaviour
 
     private void OnGUI() //allows us to run Events
     {
-        //e.keycode DOESNT store SHIFT KEYS
         string newKey = "";
-        Event e = Event.current;
-        //when currentKey us not nnunll, that means we want to reassign a key
+        Event e = Event.current; //e is now the current GUI event
+
         if (currentKey != null)//only when we are trying to reassign a key, so when we click a button, currentKey will NOT equal null
         {
-            if (e.isKey)
+            if (e.isKey) //if what happened is a key was pressed
             {
                 //this is any key right now
                 newKey = e.keyCode.ToString();
+                //e.keycode DOESNT store SHIFT KEYS
             }
 
             //There is an issue getting shift keys, the following will fix that
-            if(Input.GetKey(KeyCode.LeftShift))
+            if (Input.GetKey(KeyCode.LeftShift)) 
             {
-                newKey = "LeftShift";
+                newKey = "LeftShift"; //set key as Left Shift
             }
             if (Input.GetKey(KeyCode.RightShift))
             {
-                newKey = "RightShift";
+                newKey = "RightShift"; //set key as right shift
             }
-            if(newKey != "") //if we have a set key
+
+            #region My Double Assignment Code
+            if (newKey != "") //this is done twice, first is to test if its an assigned key, 
+            {
+                foreach (var key in keys) //var as each element of keys is a complex data type
+                {
+                    if (key.Value.ToString() == newKey && newKey != (keys[currentKey.name]).ToString()) //(keys[currentKey.name]).ToString(); || currentKey.GetComponentInChildren<Text>().text; Both work
+                    {
+                        Debug.LogWarning("That key is already assigned");
+                        currentKey.GetComponent<Image>().color = unassignedKey; //gives a color to notify user key is now unassigned
+                        newKey = "";
+                        currentKey = null;
+                    }
+                }
+            }
+            #endregion
+
+            if (newKey != "") //if we have a set key
             {
                 //we change our dictionary (that means our keybind changes too)
+                //as buttons were named correctly after the string values of the dictonary, currentKey.name returns a keys key name
+                //that key of string now has the value, KeyCode.newKey, e.g. KeyCode.P
                 keys[currentKey.name] = (KeyCode)System.Enum.Parse(typeof(KeyCode), newKey);
                 //getting the text component in our button and changing the text
-                currentKey.GetComponentInChildren<Text>().text = newKey;
-                currentKey.GetComponent<Image>().color = changedKey; //gives a color to notify user key ISNT the default
-                currentKey = null;
+                currentKey.GetComponentInChildren<Text>().text = newKey; //changes Text of the child of the button clicked to newKey, e.g. P
+                currentKey.GetComponent<Image>().color = changedKey; //gives a color to notify user key has been changed
+                currentKey = null; //sets currentKey back to null to reset conditionals
             }
         }
     }
-    public void ChangeKey(GameObject clickKey) // used as an onclick
+    public void ChangeKey(GameObject clickKey) // used as an onclick method, clickKey is the button we clicked in GUI, method is connected to the UI button, so works on press
     {
-        //gets connected to the UI button so works on press
-        currentKey = clickKey;
-        if (clickKey != null)
+        currentKey = clickKey; //current button
+
+        if (clickKey != null) //if we have clicked a key and its selected
         {
-            currentKey.GetComponent<Image>().color = selectedKey;//gives us a visual update that this key is currently selected for changing
+            currentKey.GetComponent<Image>().color = selectedKey;//gives us a visual update (changes the button color) that this key is currently selected for changing
         }
     }
 
     public void SaveKeys()
     {
 
-        foreach(var key in keys) //
+        foreach(var key in keys) //var because keys are a complex data type.
         {
             PlayerPrefs.SetString(key.Key, key.Value.ToString());
         }
 
     }
+
+    /*public string CheckClashWithBinds(string currentKey, string newKey)
+    {
+        foreach(var key in keys) //var
+        {
+            if(key.Value.ToString() == newKey && currentKey != newKey)
+            {
+                Debug.LogError("That key is already assigned");
+                newKey = "";
+            }
+        }
+        return newKey;
+    }*/
 }
