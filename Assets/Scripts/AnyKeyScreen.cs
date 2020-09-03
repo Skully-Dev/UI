8d05835360a63a958125ec5b99c3a8e559324304
Skully <69292508+Skully-13@users.noreply.github.com>
2020-09-03 16:16:25 +1000

    small clean up

--- Assets/Scripts/AnyKeyScreen.cs
+++ Assets/Scripts/AnyKeyScreen.cs
@@ -1,29 +1,15 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class AnyKeyScreen : MonoBehaviour
 {
     public GameObject anyKeyScreen;
     public GameObject mainMenu;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     private void OnGUI()
     {
         Event e = Event.current; //e is now the current GUI event
 
-        if (e.isKey || e.isMouse) //if what happened is a key was pressed
+        if (e.isKey || e.isMouse) //if what happened is a key was pressed or mouse was clicked
         {
             //if any key or mouse is pressed
             anyKeyScreen.SetActive(false);
--- Assets/Scripts/OptionsMenu.cs
+++ Assets/Scripts/OptionsMenu.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;//for lists
+﻿using System.Collections.Generic;//for lists
 using UnityEngine;
 using UnityEngine.UI;
 
