1a6119720dc0c6c445c2fc7ab09b8a107688c483
Skully <69292508+Skully-13@users.noreply.github.com>
2020-09-26 19:33:27 +1000

    Custom Character

---
+++ Assets/MiniMap.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiniMap : MonoBehaviour
+{
+    //move minimap without rotations
+
+    [SerializeField]
+    private GameObject player;
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.position = new Vector3(player.transform.position.x, this.transform.position.y, player.transform.position.z);     }
+}
