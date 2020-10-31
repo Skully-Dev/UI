136a4f0a4da41f11747a2ee1527849bf667a3c5c
Skully <69292508+Skully-13@users.noreply.github.com>
2020-10-31 15:28:12 +1100

    IMGUI items

---
+++ Assets/Scripts/Inventory/Item.cs
@@ -0,0 +1,112 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum ItemType
+{
+    Food,
+    Weapon,
+    Apparel,
+    Crafting,
+    Ingredients,
+    Potions,
+    Scrolls,
+    Quest,
+    Money
+}
+
+[System.Serializable]//debug
+public class Item
+{
+    //serialized for debug
+    #region Private Variables
+    private int id;
+    [SerializeField]private string name;
+    [SerializeField]private string description;
+    [SerializeField]private int value;
+    [SerializeField]private int amount;
+    [SerializeField]private Texture2D icon;
+    [SerializeField]private GameObject mesh;
+    [SerializeField]private ItemType type;
+    [SerializeField] private int damage;//
+    [SerializeField] private int armour;// only ever need one of the 3, so could be replaced with effect amount and apply based on item type
+    [SerializeField] private int heal;//
+    #endregion
+
+    #region Public Properties
+    public int ID
+    {
+        get { return id; }
+        set { id = value; }
+    }
+    public string Name
+    {
+        get { return name; }
+        set { name = value; }
+    }
+    public string Description
+    {
+        get { return description; }
+        set { description = value; }
+    }
+    public int Value
+    {
+        get { return value; }
+        set { this.value = value; }
+    }
+    public int Amount
+    {
+        get { return amount; }
+        set { amount = value; }
+    }
+    public Texture2D Icon
+    {
+        get { return icon; }
+        set { icon = value; }
+    }
+    public GameObject Mesh
+    {
+        get { return mesh; }
+        set { mesh = value; }
+    }
+    public ItemType Type
+    {
+        get { return type; }
+        set { type = value; }
+    }
+    public int Damage
+    {
+        get { return damage; }
+        set { damage = value; }
+    }
+    public int Armour
+    {
+        get { return armour; }
+        set { armour = value; }
+    }
+    public int Heal
+    {
+        get { return heal; }
+        set { heal = value; }
+    }
+    #endregion
+
+    public Item()
+    {
+
+    }
+
+    public Item(Item copyItem, int copyAmount)
+    {
+        name = copyItem.Name;
+        description = copyItem.Description;
+        value = copyItem.value;
+        amount = copyAmount;
+        icon = copyItem.Icon;
+        mesh = copyItem.Mesh; //as they are prefabs, this is fine, but if it was an item in scene, instantiate would be a better solution
+        type = copyItem.Type;
+        damage = copyItem.Damage;
+        armour = copyItem.Armour;
+        heal = copyItem.Heal;
+    }
+}
---
+++ Assets/Scripts/Inventory/Shop.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shop : MonoBehaviour
+{
+    public List<Item> shopInventory = new List<Item>();
+    private Item selectedItem;
+
+    private Inventory playerInventory;
+
+    #region Display shop Variables
+    [SerializeField] private bool showShop = false;
+    private Vector2 scr;
+    //There might be some Dialogue
+
+    #endregion
+
+    private void Start()
+    {
+        playerInventory = (Inventory) FindObjectOfType<Inventory>();
+
+        if (playerInventory == null)
+        {
+            Debug.LogError("There is no player with an inventory in this scene");
+        }
+
+    }
+
+
+    private void OnGUI()
+    {
+        scr.x = Screen.width / 16;
+        scr.y = Screen.height / 9;
+
+        if (showShop)
+        {
+            for (int i = 0; i < shopInventory.Count; i++)
+            {
+                if (GUI.Button(new Rect(12.5f * scr.x, (0.25f * scr.y) + i * (0.25f * scr.y),
+                                         3 * scr.x, .25f * scr.y), shopInventory[i].Name))
+                {
+                    selectedItem = shopInventory[i];
+                }
+            }
+
+            if (selectedItem != null)
+            {
+                GUI.Box(new Rect(8.5f * scr.x, 0.25f * scr.y,
+                                    3.5f * scr.x, 7 * scr.y), "");
+                GUI.Box(new Rect(8.75f * scr.x, 0.5f * scr.y,
+                                    3 * scr.x, 3 * scr.y), selectedItem.Icon);
+                GUI.Box(new Rect(9.05f * scr.x, 3.5f * scr.y,
+                                    2.5f * scr.x, .5f * scr.y), selectedItem.Name);
+                GUI.Box(new Rect(8.75f * scr.x, 4 * scr.y, 3 * scr.x, 3 * scr.y),
+                                    selectedItem.Description + "\nValue: " +
+                                    selectedItem.Value + "\nAmount: " + selectedItem.Amount);
+
+                if (playerInventory.money >= selectedItem.Value)
+                {
+                    if (GUI.Button(new Rect(10.5f * scr.x, 6.5f * scr.y, scr.x, 0.25f * scr.y), "Purchase Item"))
+                    {
+                        playerInventory.money -= selectedItem.Value; //maybe convert money variable by removing and just using objects of type money, where you give money objects to buy and get money objects to sell.
+
+                        //add to player
+                        playerInventory.AddItem(selectedItem);
+
+                        //remove from shop
+                        selectedItem.Amount--;
+                        if (selectedItem.Amount <= 0)
+                        {
+                            shopInventory.Remove(selectedItem);
+                            selectedItem = null;
+                        }
+                    }
+                }
+            }
+
+            //display players inv
+            playerInventory.showInventory = true;
+
+            if (GUI.Button(new Rect(.25f * scr.x, 8.5f * scr.y, scr.x, .5f * scr.y), "Exit Shop"))
+            {
+                playerInventory.showInventory = false;
+                showShop = false;
+            }
+        }
+    }
+}
